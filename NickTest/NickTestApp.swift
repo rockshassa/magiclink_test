f005150168d8bce4a025b599c8ec45501c1ff843
nick <nick@floornfts.io>
2023-01-19 12:27:11 -0500

    Initial Commit

---
+++ NickTest/NickTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NickTestApp.swift
+//  NickTest
+//
+//  Created by Nick on 1/19/23.
+//
+
+import SwiftUI
+
+@main
+struct NickTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
